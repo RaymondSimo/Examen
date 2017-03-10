50fb2dbf03571069ad3009b886deda29a4aec30d
RaymondSimo <raymondsimoly@gmail.com>
2017-03-10 07:59:34 -0600

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$cn=mysql_connect('am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','x4zgv7cwfuaergf6','z3vozuor0t984sxs')or die("Error en Conexion");
+$db=mysql_select_db('iis320fhcpx25b6d')or die("Error en Db");
+return($cn);
+return($db);
+?>
+
